11dcb83beba3b662db874cbba4fc378a10ae0ab3
Andres Noboa <andresnoboa17@hotmail.com>
2021-06-01 20:26:59 -0500

    Added Scrollview to the list

--- lib/widgets/transactionList.dart
+++ lib/widgets/transactionList.dart
@@ -8,38 +8,41 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.userTransactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: userTransactions.map((transaction) {
-        return Card(
-            child: Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              child: Text('\$ ${transaction.amount}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple)),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.purple, width: 2),
-              ),
-              padding: EdgeInsets.all(10),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(transaction.title,
+    return Container(
+      height: 300,
+      child: ListView(
+        children: userTransactions.map((transaction) {
+          return Card(
+              child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: Text('\$ ${transaction.amount}',
                     style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    )),
-                Text(DateFormat.yMMMMd().format(transaction.date),
-                    style: TextStyle(fontSize: 12, color: Colors.grey)),
-              ],
-            ),
-          ],
-        ));
-      }).toList(),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple)),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width: 2),
+                ),
+                padding: EdgeInsets.all(10),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(transaction.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(DateFormat.yMMMMd().format(transaction.date),
+                      style: TextStyle(fontSize: 12, color: Colors.grey)),
+                ],
+              ),
+            ],
+          ));
+        }).toList(),
+      ),
     );
   }
 }
