e056f3859c4ac4440e43222ffbcd5b6b870d8443
Andres Noboa <andresnoboa17@hotmail.com>
2021-06-10 02:01:12 -0500

    Calculating the size dynamically

--- lib/widgets/transactionList.dart
+++ lib/widgets/transactionList.dart
@@ -9,87 +9,50 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.userTransactions, this.deleteTransaction);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 500,
-      child: userTransactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text('No transaction added yet!',
-                    style: Theme.of(context).textTheme.headline6),
-                SizedBox(
-                  height: 18,
-                ),
-                Container(
-                    height: 200,
-                    child: Image.asset('assets/images/waiting.png',
-                        fit: BoxFit.cover)),
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6),
-                          child: FittedBox(
-                            child: Text(
-                              '\$ ${userTransactions[index].amount.toStringAsFixed(2)}',
-                            ),
+    return userTransactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text('No transaction added yet!',
+                  style: Theme.of(context).textTheme.headline6),
+              SizedBox(
+                height: 18,
+              ),
+              Container(
+                  height: 200,
+                  child: Image.asset('assets/images/waiting.png',
+                      fit: BoxFit.cover)),
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            '\$ ${userTransactions[index].amount.toStringAsFixed(2)}',
                           ),
-                        )),
-                    title: Text(userTransactions[index].title,
-                        style: Theme.of(context).textTheme.headline6),
-                    subtitle: Text(
-                        DateFormat.yMMMMd()
-                            .format(userTransactions[index].date),
-                        style: TextStyle(fontSize: 12, color: Colors.grey)),
-                    trailing: IconButton(
-                        onPressed: () =>
-                            deleteTransaction(userTransactions[index].id),
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor),
-                  ),
-                );
-                /*
-                Card(
-                    child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      child: Text(
-                          '\$ ${userTransactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColorDark)),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Theme.of(context).primaryColorLight,
-                            width: 2),
-                      ),
-                      padding: EdgeInsets.all(10),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(userTransactions[index].title,
-                            style: Theme.of(context).textTheme.headline6),
-                        Text(
-                            DateFormat.yMMMMd()
-                                .format(userTransactions[index].date),
-                            style: TextStyle(fontSize: 12, color: Colors.grey)),
-                      ],
-                    ),
-                  ],
-                ));*/
-              },
-              itemCount: userTransactions.length,
-            ),
-    );
+                        ),
+                      )),
+                  title: Text(userTransactions[index].title,
+                      style: Theme.of(context).textTheme.headline6),
+                  subtitle: Text(
+                      DateFormat.yMMMMd().format(userTransactions[index].date),
+                      style: TextStyle(fontSize: 12, color: Colors.grey)),
+                  trailing: IconButton(
+                      onPressed: () =>
+                          deleteTransaction(userTransactions[index].id),
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor),
+                ),
+              );
+            },
+            itemCount: userTransactions.length,
+          );
   }
 }
